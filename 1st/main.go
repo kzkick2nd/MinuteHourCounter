aeb0e3e2965e1440632e730c8907370969b4d66a
kzkick2nd <aruga.kazuki@gmail.com>
2018-11-01 21:25:50 +0900

    Plan interface

--- 1st/main.go
+++ 1st/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+	var e Events
+	e.Add(1)
+	e.Add(2)
+	e.Add(3)
+	e.MinuteCount()
+}
+
+type event struct {
+	count int
+	time  time.Time
+}
+
+type Events struct {
+	event []event
+}
+
+func (e *Events) Add(count int) {
+	e.event = append(e.event, event{count, time.Now()})
+}
+
+func (e *Events) MinuteCount() int {
+	fmt.Println(e.event)
+	return len(e.event)
+}
+
+func (e *Events) HourCount() int {
+	fmt.Println(e.event)
+	return len(e.event)
 }
