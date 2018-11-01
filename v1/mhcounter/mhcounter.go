929a82f84ff4a4127bd537390aec9527c1bd85a2
kzkick2nd <aruga.kazuki@gmail.com>
2018-11-02 01:37:21 +0900

    Refactor duplicate methods

--- v1/mhcounter/mhcounter.go
+++ v1/mhcounter/mhcounter.go
@@ -11,28 +11,24 @@ type Events struct {
 	event []event
 }
 
+func (e *Events) countSince(cutoff time.Time) int {
+	var count int
+	for _, v := range e.event {
+		if v.time.After(cutoff) {
+			count += v.count
+		}
+	}
+	return count
+}
+
 func (e *Events) Add(count int) {
 	e.event = append(e.event, event{count, time.Now()})
 }
 
 func (e *Events) MinuteCount() int {
-	var count int
-	var current = time.Now()
-	for _, v := range e.event {
-		if v.time.After(current.Add(-1 * time.Minute)) {
-			count += v.count
-		}
-	}
-	return count
+	return e.countSince(time.Now().Add(-1 * time.Minute))
 }
 
 func (e *Events) HourCount() int {
-	var count int
-	var current = time.Now()
-	for _, v := range e.event {
-		if v.time.After(current.Add(-1 * time.Hour)) {
-			count += v.count
-		}
-	}
-	return count
+	return e.countSince(time.Now().Add(-1 * time.Hour))
 }
