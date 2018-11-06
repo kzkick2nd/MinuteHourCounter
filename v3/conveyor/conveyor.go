98c6cffe3517620b90f3931fb838438ef00496f6
kzkick2nd <aruga.kazuki@gmail.com>
2018-11-06 21:41:26 +0900

    Done add function

--- v3/conveyor/conveyor.go
+++ v3/conveyor/conveyor.go
@@ -10,11 +10,31 @@ func NewBuckets(maxItems int) *QueneBuckets {
 	return &QueneBuckets{maxItems: maxItems}
 }
 
-func (q *QueneBuckets) AddToBack(count int) {
-	q.totalSum += count
+func (q *QueneBuckets) Shift(shifted int) {
+	if shifted > q.maxItems {
+		q.quene = []int{}
+		q.totalSum = 0
+		return
+	}
+
+	for shifted > 0 {
+		q.quene = append(q.quene, 0)
+		shifted--
+	}
+
+	for len(q.quene) > q.maxItems {
+		q.totalSum -= q.quene[0]
+		q.quene = q.quene[1:]
+	}
 }
 
-func (q *QueneBuckets) Shift(shifted int) {}
+func (q *QueneBuckets) AddToBack(count int) {
+	if len(q.quene) == 0 {
+		q.Shift(1)
+	}
+	q.quene[len(q.quene)-1] += count
+	q.totalSum += count
+}
 
 func (q *QueneBuckets) TotalSum() int {
 	return q.totalSum
