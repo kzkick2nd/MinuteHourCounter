e094699ad7cfd03592b8dede79e04c80fb725b9b
kzkick2nd <aruga.kazuki@gmail.com>
2018-12-31 17:18:01 +0900

    Add v3 comments

--- v3/mhcounter/mhcounter.go
+++ v3/mhcounter/mhcounter.go
@@ -1,3 +1,5 @@
+// Package mhcounter track the cumulative counts over the past minute and over the past hour.
+// Useful, for example, to track recent bandwidth usage.
 package mhcounter
 
 import (
@@ -25,17 +27,22 @@ func NewCounter() *Counter {
 	}
 }
 
+// Add a new data point (count >= 0).
+// For the next minute, MinuteCount() will be larger by +count.
+// For the next hour, HourCount() will be larger by +count.
 func (c *Counter) Add(count int) {
 	var now = time.Now()
 	c.minuteCounts.Add(count, now)
 	c.hourCounts.Add(count, now)
 }
 
+// MinuteCount returns the accumulated count over the past 60 seconds.
 func (c *Counter) MinuteCount() int {
 	var now = time.Now()
 	return c.minuteCounts.TrailingCount(now)
 }
 
+// HourCount returns the accumulated count over the past 3600 seconds.
 func (c *Counter) HourCount() int {
 	var now = time.Now()
 	return c.hourCounts.TrailingCount(now)
